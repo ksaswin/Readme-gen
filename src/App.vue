d83ee1616f2636f0bafc42dd5dc97ffa3e0ee6d6
Aswin-Shaji <aswin2000.ak@gmail.com>
2022-03-23 17:17:42 +0530

    Initial setup.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <mark-down></mark-down>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MarkDown from "./components/MarkDown.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MarkDown,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/MarkDown.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-html="markdownToHtml"></div>
+</template>
+
+<script>
+import { marked } from "marked";
+
+export default {
+  name: "MarkDown",
+  data() {
+    return {
+      markdown: {
+        title: `
+# Project Title
+
+A brief description of what this project does and who it's for
+
+`,
+      },
+    };
+  },
+  computed: {
+    markdownToHtml() {
+      return marked(this.markdown.title);
+    },
+  },
+};
+</script>
