50a605368bd7db2d95972c5f04c3adc553a8efb8
Aswin-Shaji <aswin2000.ak@gmail.com>
2022-03-24 15:46:14 +0530

    Added markdown editor and preview sections.

--- src/components/MarkDown.vue
+++ src/components/MarkDown.vue
@@ -1,28 +1,77 @@
 <template>
-  <div v-html="markdownToHtml"></div>
+  <div class="markdown-containers">
+    <div class="container">
+      <h4 class="editor">Editor</h4>
+      <textarea v-model="sections.title.content"></textarea>
+    </div>
+    <div class="container">
+      <div class="view-changer">
+        <h4>Preview</h4>
+        <h4>Raw</h4>
+      </div>
+      <div v-html="markdownToHtml"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { marked } from "marked";
+import { sections } from "@/defaults";
 
 export default {
   name: "MarkDown",
+  props: {
+    darkmode: Boolean,
+  },
   data() {
     return {
-      markdown: {
-        title: `
-# Project Title
-
-A brief description of what this project does and who it's for
-
-`,
-      },
+      sections,
     };
   },
   computed: {
     markdownToHtml() {
-      return marked(this.markdown.title);
+      return marked(this.sections.title.content);
     },
   },
 };
 </script>
+
+<style scoped>
+.markdown-containers {
+  display: flex;
+  height: 90vh;
+}
+
+.container {
+  /* background: rgb(218, 218, 218); */
+  flex-basis: 50%;
+  padding: 5px;
+  margin: 5px;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  box-sizing: border-box;
+}
+
+.editor {
+  margin: none;
+}
+
+.view-changer {
+  display: flex;
+}
+
+.view-changer h4 {
+  margin: 10px;
+}
+
+textarea {
+  font-size: 15px;
+  resize: none;
+  border: rgb(200, 200, 200);
+  outline: none;
+  background-color: rgb(38, 38, 38);
+  color: rgb(200, 200, 200);
+  height: 100%;
+}
+</style>
