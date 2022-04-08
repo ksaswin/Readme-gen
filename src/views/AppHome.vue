dcc7aa5aa698874ed9541720d2e785525e13ce0b
Aswin-Shaji <aswin2000.ak@gmail.com>
2022-04-08 15:15:20 +0530

    Created a Home page for the app.

---
+++ src/views/AppHome.vue
@@ -0,0 +1,102 @@
+<template>
+  <!-- Top header section starts here -->
+  <div class="header-wrapper">
+    <!-- Title and subtitle -->
+    <div class="app-name">
+      <router-link :to="{ name: 'Home' }" class="md-logo">
+        <img src="@/assets/icons/markdown.png" alt="readme gen logo" />
+      </router-link>
+      <p>A <a href="https://readme.so/editor">readme.so</a> clone</p>
+    </div>
+    <!-- Title and subtitle -->
+
+    <!-- Github link -->
+    <div class="header-buttons-div">
+      <button class="header-btn">
+        <a href="https://github.com/ksaswin/Readme-gen">
+          <img class="github-img" src="@/assets/icons/github.svg" alt=""
+        /></a>
+      </button>
+    </div>
+    <!-- Github link -->
+  </div>
+  <!-- Top header section starts here -->
+
+  <!-- Home content starts here -->
+  <div class="home-content">
+    <!-- Main title -->
+    <h1 class="text main-title">
+      <span> The easiest way to create a </span>
+      <span class="readme"> README </span>
+    </h1>
+    <!-- Main title -->
+
+    <!-- Sub title -->
+    <h3 class="text sub-title">
+      <span>
+        This simple editor allows you to quickly add and customize all the
+        sections your need for
+      </span>
+      <span>your project's readme.</span>
+    </h3>
+    <!-- Sub title -->
+
+    <!-- Link to editor page -->
+    <router-link :to="{ name: 'Editor' }">
+      <button class="start-btn">
+        <h4 class="text-btn">Get Started</h4>
+      </button>
+    </router-link>
+    <!-- Link to editor page -->
+  </div>
+  <!-- Home content ends here -->
+</template>
+
+<style scoped>
+.home-content {
+  font-family: Arial, Helvetica, sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-top: 60px;
+}
+
+.text {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 0 30px 0;
+}
+
+.main-title {
+  font-size: 7vh;
+  color: white;
+}
+
+.readme {
+  color: rgb(84, 181, 132);
+}
+
+.sub-title {
+  font-size: 2vh;
+  font-weight: 300;
+  margin-bottom: 40px;
+}
+
+.start-btn {
+  height: 4vw;
+  width: 18vw;
+
+  border: none;
+  border-radius: 10px;
+  background: rgb(84, 181, 132);
+  cursor: pointer;
+}
+
+.text-btn {
+  margin: 0;
+  color: white;
+  font-size: 3vh;
+}
+</style>
