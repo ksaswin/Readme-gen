7a1bbf604e5595e7d9a86697361a9ea1c3c75834
Aswin <aswin2000.ak@gmail.com>
2023-03-13 19:52:47 +0530

    Refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import copyText from "@meforma/vue-copy-to-clipboard";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import copyText from '@meforma/vue-copy-to-clipboard';
 
-createApp(App).use(copyText).use(router).mount("#app");
+
+const app = createApp(App);
+
+app.use(router);
+app.use(copyText);
+
+app.mount('#app');
+
