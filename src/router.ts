e386c1e33cac3cdc8bb92548b15fdf9207dddbc8
Aswin <aswin2000.ak@gmail.com>
2023-03-13 19:54:28 +0530

    Modify component imports in router

--- src/router.ts
+++ src/router.ts
@@ -1,28 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const AppHome = () =>
-  import(/* webpackChunkName: "AppHome" */ "@/views/AppHome.vue");
-const AppEditor = () =>
-  import(/* webpackChunkName: "AppEditor" */ "@/views/AppEditor.vue");
-
-const NotFound = () =>
-  import(/* webpackChunkName: "NotFound" */ "@/views/NotFound.vue");
 
 const routes = [
   {
     path: "/",
     name: "Home",
-    component: AppHome,
+    component: () => import('@/views/AppHome.vue')
   },
   {
     path: "/editor",
     name: "Editor",
-    component: AppEditor,
+    component: () => import('@/views/AppEditor.vue')
   },
   {
     path: "/:pathName(.*)*",
     name: "NotFound",
-    component: NotFound,
+    component: () => import('@/views/NotFound.vue')
   },
 ];
 
