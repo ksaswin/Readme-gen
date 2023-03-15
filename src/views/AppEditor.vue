2a78c6832aa8e1994ce23d71f810c030ca98dda1
Aswin <aswin2000.ak@gmail.com>
2023-03-15 17:43:43 +0530

    Update component script to reactive ts

--- src/views/AppEditor.vue
+++ src/views/AppEditor.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="main" :style="lightModeSet ? lightMode : ''">
-    <mark-down :isLight="lightModeSet"></mark-down>
+  <div class="main" :class='{ light: isLightModeSet }'>
+    <mark-down :isLight="isLightModeSet"></mark-down>
   </div>
 </template>
 
-<script>
+<script setup lang='ts'>
+import { computed } from 'vue';
+
+import { useMdStore } from '@/store/mdstore';
 import MarkDown from "@/components/MarkDown.vue";
 
-export default {
-  name: "App",
-  components: {
-    MarkDown,
-  },
-  data() {
-    return {
-      lightModeSet: false,
-      lightMode: {
-        background: "rgb(220, 220, 220)",
-        color: "rgb(38, 38, 38)",
-      },
-    };
-  },
-};
+const store = useMdStore();
+
+const isLightModeSet = computed((): boolean => {
+  return store.isLightModeEnabled;
+});
 </script>
+
+<style>
+.light {
+  background: rgb(220, 220, 220);
+  color: rgb(38, 38, 38);
+}
+</style>
 
