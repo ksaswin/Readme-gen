b175d3c1505ddddca46a4d2bceb7d267a9b4631c
Aswin <aswin2000.ak@gmail.com>
2023-03-15 17:28:26 +0530

    Update main.ts to use pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import copyText from '@meforma/vue-copy-to-clipboard';
@@ -6,6 +7,7 @@ import copyText from '@meforma/vue-copy-to-clipboard';
 
 const app = createApp(App);
 
+app.use(createPinia());
 app.use(router);
 app.use(copyText);
 
