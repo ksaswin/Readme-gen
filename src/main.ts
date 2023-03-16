ae46a24bab1eafc5a63d40e94589a7be7295b266
Aswin <aswin2000.ak@gmail.com>
2023-03-16 13:31:41 +0530

    Remove copy-to-clipboard package

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,12 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
-import copyText from '@meforma/vue-copy-to-clipboard';
 
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(copyText);
 
 app.mount('#app');
 
