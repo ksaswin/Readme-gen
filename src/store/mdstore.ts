de7e6617550b9fbe92098aadfd3466c38841521b
Aswin <aswin2000.ak@gmail.com>
2023-03-17 09:58:38 +0530

    Add mutation actions and getters for usedSections state in mdstore

--- src/store/mdstore.ts
+++ src/store/mdstore.ts
@@ -20,6 +20,7 @@ export const useMdStore = defineStore('mdstore', {
     availableSections: sections[1],
     newSectionId: INITIAL_NEW_SECTION_ID
   }),
+
   actions: {
     toggleLightMode(): void {
       this.isLightModeEnabled = !this.isLightModeEnabled;
@@ -27,7 +28,55 @@ export const useMdStore = defineStore('mdstore', {
     
     incrementId(): void {
       this.newSectionId++;
+    },
+
+    updateUsedSectionContent(index: number, updateContent: string=''): void {
+      this.usedSections[index].content = updateContent;
+    },
+
+    addSectionToUsedSections(section: Section): void {
+      this.removeSectionFromAvailableSections(section);
+
+      section.selected = true;
+
+      this.usedSections.push(section);
+    },
+
+    updateSelectedFlagInSection(index: number, flag: boolean): void {
+      this.usedSections[index].selected = flag;
+    },
+    
+    spliceUsedSection(index: number, deleteCount: number=1, item?: Section): void {
+      if (item !== undefined) {
+        this.usedSections.splice(index, deleteCount, item);
+      } else {
+        this.usedSections.splice(index, deleteCount);
+      }
+    },
+
+    slicedUsedSectionContent(index: number, sliceStart: number, sliceEnd?: number): string {
+      if (sliceEnd !== undefined) {
+        return this.usedSections[index].content.slice(sliceStart, sliceEnd)
+      } else {
+        return this.usedSections[index].content.slice(sliceStart)
+      }
+    },
+
+    removeSectionFromAvailableSections(section: Section): void {
+      for (let i = 0; i < this.availableSections.length; i++) {
+        if (section.id === this.availableSections[i].id) {
+          this.availableSections.splice(i, 1);
+
+          break;
+        }
+      }
     }
+  },
+
+  getters: {
+    usedSectionsLength: (state) => {
+      return state.usedSections.length;
+    },
   }
 });
 
