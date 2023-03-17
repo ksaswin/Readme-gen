0ab36abac34d1ef9bf8d9defac9b725d98d182ff
Aswin <aswin2000.ak@gmail.com>
2023-03-17 12:12:16 +0530

    Modify defaults model

--- src/models/defaults.ts
+++ src/models/defaults.ts
@@ -1,23 +1,26 @@
-export const sections = [
-  [
-    {
-      selected: true,
-      id: 30,
-      name: "Title and Description",
-      content: `
+import type { Section } from '@/models/sections';
+
+
+export const usedSections: Array<Section> = [
+  {
+    selected: true,
+    id: 30,
+    name: "Title and Description",
+    content: `
 # Project Title
 
 A brief description of what this project does and who it's for
 
 `,
-    },
-  ],
-  [
-    {
-      selected: false,
-      id: 1,
-      name: "Acknowledgements",
-      content: `
+  }
+];
+
+export const availableSections: Array<Section> = [
+  {
+    selected: false,
+    id: 1,
+    name: "Acknowledgements",
+    content: `
 ## Acknowledgements
 
 - [Awesome Readme Templates](https://awesomeopensource.com/project/elangosundar/awesome-README-templates)
@@ -25,34 +28,34 @@ A brief description of what this project does and who it's for
 - [How to write a Good readme](https://bulldogjob.com/news/449-how-to-write-a-good-readme-for-your-github-project)
 
 `,
-    },
-    {
-      selected: false,
-      id: 2,
-      name: "Appendix",
-      content: `
+  },
+  {
+    selected: false,
+    id: 2,
+    name: "Appendix",
+    content: `
 ## Appendix
 
 Any additional information goes here
 
 `,
-    },
-    {
-      selected: false,
-      id: 3,
-      name: "Authors",
-      content: `
+  },
+  {
+    selected: false,
+    id: 3,
+    name: "Authors",
+    content: `
 ## Authors
 
 - [@ksaswin](https://github.com/ksaswin)
 
 `,
-    },
-    {
-      selected: false,
-      id: 4,
-      name: "Color Reference",
-      content: `## Color Reference
+  },
+  {
+    selected: false,
+    id: 4,
+    name: "Color Reference",
+    content: `## Color Reference
 
 | Color             | Hex                                                                |
 | ----------------- | ------------------------------------------------------------------ |
@@ -60,12 +63,12 @@ Any additional information goes here
 | Example Color | ![#f8f8f8](https://via.placeholder.com/10/f8f8f8?text=+) #f8f8f8 |
 | Example Color | ![#00b48a](https://via.placeholder.com/10/00b48a?text=+) #00b48a |
 | Example Color | ![#00d1a0](https://via.placeholder.com/10/00b48a?text=+) #00d1a0 |\n`,
-    },
-    {
-      selected: false,
-      id: 5,
-      name: "Contributing",
-      content: `
+  },
+  {
+    selected: false,
+    id: 5,
+    name: "Contributing",
+    content: `
 ## Contributing
 
 Contributions are always welcome!
@@ -74,55 +77,55 @@ See contributing.md for ways to get started.
 
 Please adhere to this project's code of conduct.
 `,
-    },
-    {
-      selected: false,
-      id: 6,
-      name: "Demo",
-      content: `
+  },
+  {
+    selected: false,
+    id: 6,
+    name: "Demo",
+    content: `
 ## Demo
 
 Insert gif or link to demo
 
 `,
-    },
-    {
-      selected: false,
-      id: 7,
-      name: "Deployment",
-      content: `
+  },
+  {
+    selected: false,
+    id: 7,
+    name: "Deployment",
+    content: `
 ## Deployment
 
 To deploy this project run the following code.
 `,
-    },
-    {
-      selected: false,
-      id: 8,
-      name: "Deployment",
-      content: `
+  },
+  {
+    selected: false,
+    id: 8,
+    name: "Deployment",
+    content: `
 ## Documentation
 
 [Documentation](https://linktodocumentation)
 
 `,
-    },
-    {
-      selected: false,
-      id: 9,
-      name: "Environment Variables",
-      content: `
+  },
+  {
+    selected: false,
+    id: 9,
+    name: "Environment Variables",
+    content: `
 ## Environment Variables
 
 To run this project, you will need to add the following environment variables to your .env file
 
 `,
-    },
-    {
-      selected: false,
-      id: 10,
-      name: "FAQ",
-      content: `
+  },
+  {
+    selected: false,
+    id: 10,
+    name: "FAQ",
+    content: `
 ## FAQ
 
 #### Question 1
@@ -134,12 +137,12 @@ Answer 1
 Answer 2
 
 `,
-    },
-    {
-      selected: false,
-      id: 11,
-      name: "Features",
-      content: `
+  },
+  {
+    selected: false,
+    id: 11,
+    name: "Features",
+    content: `
 ## Features
 
 - Light/dark mode toggle
@@ -148,54 +151,54 @@ Answer 2
 - Cross platform
 
 `,
-    },
-    {
-      selected: false,
-      id: 12,
-      name: "Feedback",
-      content: `
+  },
+  {
+    selected: false,
+    id: 12,
+    name: "Feedback",
+    content: `
 ## Feedback
 
 If you have any feedback, please reach out to us at fake@fake.com
 
 `,
-    },
-    {
-      selected: false,
-      id: 13,
-      name: "Github Profile - About Me",
-      content: `
+  },
+  {
+    selected: false,
+    id: 13,
+    name: "Github Profile - About Me",
+    content: `
 ## 🚀 About Me
 I'm a full stack developer...
 
 `,
-    },
-    {
-      selected: false,
-      id: 14,
-      name: "Github Profile - Introduction",
-      content: `
+  },
+  {
+    selected: false,
+    id: 14,
+    name: "Github Profile - Introduction",
+    content: `
 # Hi, I'm Aswin! 👋
 
 `,
-    },
-    {
-      selected: false,
-      id: 15,
-      name: "Github Profile - Links",
-      content: `
+  },
+  {
+    selected: false,
+    id: 15,
+    name: "Github Profile - Links",
+    content: `
 ## 🔗 Links
 [![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://portfolio.com/)
 [![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/)
 [![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/)
 
 `,
-    },
-    {
-      selected: false,
-      id: 16,
-      name: "Github Profile - Other",
-      content: `
+  },
+  {
+    selected: false,
+    id: 16,
+    name: "Github Profile - Other",
+    content: `
 ## Other Common Github Profile Sections
 👩‍💻 I'm currently working on...
 
@@ -214,74 +217,74 @@ I'm a full stack developer...
 ⚡️ Fun fact...
 
 `,
-    },
-    {
-      selected: false,
-      id: 17,
-      name: "Github Profile - Skills",
-      content: `
+  },
+  {
+    selected: false,
+    id: 17,
+    name: "Github Profile - Skills",
+    content: `
 ## 🛠 Skills
 Javascript, HTML, CSS...
 
 `,
-    },
-    {
-      selected: false,
-      id: 18,
-      name: "Installation",
-      content: `
+  },
+  {
+    selected: false,
+    id: 18,
+    name: "Installation",
+    content: `
 ## Installation
 
 Install my-project with npm
 `,
-    },
-    {
-      selected: false,
-      id: 19,
-      name: "Lessons",
-      content: `
+  },
+  {
+    selected: false,
+    id: 19,
+    name: "Lessons",
+    content: `
 ## Lessons Learned
 
 What did you learn while building this project? What challenges did you face and how did you overcome them?
 
 `,
-    },
-    {
-      selected: false,
-      id: 20,
-      name: "License",
-      content: `
+  },
+  {
+    selected: false,
+    id: 20,
+    name: "License",
+    content: `
 ## License
 
 [MIT](https://choosealicense.com/licenses/mit/)
 
 `,
-    },
-    {
-      selected: false,
-      id: 21,
-      name: "Logo",
-      content: `
+  },
+  {
+    selected: false,
+    id: 21,
+    name: "Logo",
+    content: `
 ![Logo](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/th5xamgrr6se0x5ro4g6.png)
 
 `,
-    },
-    {
-      selected: false,
-      id: 22,
-      name: "Optimizations",
-      content: `
+  },
+  {
+    selected: false,
+    id: 22,
+    name: "Optimizations",
+    content: `
 ## Optimizations
 
 What optimizations did you make in your code? E.g. refactors, performance improvements, accessibility
 
 `,
-    },
-    {
-      selected: false,
-      id: 23,
-      name: "Related",
-      content: `
+  },
+  {
+    selected: false,
+    id: 23,
+    name: "Related",
+    content: `
 ## Related
 
 Here are some related projects
@@ -289,12 +292,12 @@ Here are some related projects
 [Awesome README](https://github.com/matiassingers/awesome-readme)
 
 `,
-    },
-    {
-      selected: false,
-      id: 24,
-      name: "Road Map",
-      content: `
+  },
+  {
+    selected: false,
+    id: 24,
+    name: "Road Map",
+    content: `
 ## Roadmap
 
 - Additional browser support
@@ -302,12 +305,12 @@ Here are some related projects
 - Add more integrations
 
 `,
-    },
-    {
-      selected: false,
-      id: 25,
-      name: "Run Locally",
-      content: `
+  },
+  {
+    selected: false,
+    id: 25,
+    name: "Run Locally",
+    content: `
 ## Run Locally
 
 Clone the project
@@ -319,34 +322,34 @@ Install dependencies
 Start the server
 
 `,
-    },
-    {
-      selected: false,
-      id: 26,
-      name: "Screenshots",
-      content: `
+  },
+  {
+    selected: false,
+    id: 26,
+    name: "Screenshots",
+    content: `
 ## Screenshots
 
 ![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
 
 `,
-    },
-    {
-      selected: false,
-      id: 27,
-      name: "Support",
-      content: `
+  },
+  {
+    selected: false,
+    id: 27,
+    name: "Support",
+    content: `
 ## Support
 
 For support, email fake@fake.com or join our Slack channel.
 
 `,
-    },
-    {
-      selected: false,
-      id: 28,
-      name: "Tech",
-      content: `
+  },
+  {
+    selected: false,
+    id: 28,
+    name: "Tech",
+    content: `
 ## Tech Stack
 
 **Client:** React, Redux, TailwindCSS
@@ -354,32 +357,32 @@ For support, email fake@fake.com or join our Slack channel.
 **Server:** Node, Express
 
 `,
-    },
-    {
-      selected: false,
-      id: 29,
-      name: "Running Tests",
-      content: `
+  },
+  {
+    selected: false,
+    id: 29,
+    name: "Running Tests",
+    content: `
 ## Running Tests
 
 To run tests, run the following command
 
 `,
-    },
-    {
-      selected: false,
-      id: 31,
-      name: "Usage/Examples",
-      content: `
+  },
+  {
+    selected: false,
+    id: 31,
+    name: "Usage/Examples",
+    content: `
 ## Usage/Examples
 
 `,
-    },
-    {
-      selected: false,
-      id: 32,
-      name: "Used By",
-      content: `
+  },
+  {
+    selected: false,
+    id: 32,
+    name: "Used By",
+    content: `
 ## Used By
 
 This project is used by the following companies:
@@ -388,6 +391,5 @@ This project is used by the following companies:
 - Company 2
 
 `,
-    },
-  ],
+  },
 ];
