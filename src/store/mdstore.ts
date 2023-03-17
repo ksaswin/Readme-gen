042d612e351c4b6bcba5555a37dba426d1cf301d
Aswin <aswin2000.ak@gmail.com>
2023-03-17 12:12:45 +0530

    Modify import path for defaults model in mdstore

--- src/store/mdstore.ts
+++ src/store/mdstore.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia';
 
-import { sections } from '@/defaults';
+import { usedSections, availableSections } from '@/models/defaults';
 import type { Section } from '@/models/sections';
 
 
@@ -11,13 +11,13 @@ interface State {
   newSectionId: number
 }
 
-const INITIAL_NEW_SECTION_ID = sections[0].length + sections[1].length + 1;
+const INITIAL_NEW_SECTION_ID = usedSections.length + availableSections.length;
 
 export const useMdStore = defineStore('mdstore', {
   state: (): State => ({
     isLightModeEnabled: false,
-    usedSections: sections[0],
-    availableSections: sections[1],
+    usedSections,
+    availableSections,
     newSectionId: INITIAL_NEW_SECTION_ID
   }),
 
